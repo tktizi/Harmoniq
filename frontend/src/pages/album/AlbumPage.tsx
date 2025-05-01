259e4ded854fa41eb4b239f89b09f3a55ab1d6c2
tktizi <maks.krish7@gmail.com>
2025-05-01 20:39:21 +0300

    Play Song Functionality & Queue

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useMusicStore } from "@/stores/useMusicStore";
-import { Clock, Play } from "lucide-react";
+import { usePlayerStore } from "@/stores/usePlayerStore";
+import { Clock, Pause, Play } from "lucide-react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -13,28 +14,45 @@ export const formatDuration = (seconds: number) => {
 
 const AlbumPage = () => {
 	const { albumId } = useParams();
-    const { fetchAlbumById, currentAlbum, isLoading } = useMusicStore();
+	const { fetchAlbumById, currentAlbum, isLoading } = useMusicStore();
+	const { currentSong, isPlaying, playAlbum, togglePlay } = usePlayerStore();
 
-
-    useEffect(() => {
+	useEffect(() => {
 		if (albumId) fetchAlbumById(albumId);
 	}, [fetchAlbumById, albumId]);
 
 	if (isLoading) return null;
 
-    
-    return <div className='h-full'>
-        <ScrollArea className='h-full rounded-md'>
-            {/* Main Content */}
-            <div className='relative min-h-full'>
-                {/* bg gradient */}
-                <div
-                    className='absolute inset-0 bg-gradient-to-b from-[#008BF0]/80 via-zinc-900/80
-                to-zinc-900 pointer-events-none'
-                    aria-hidden='true'
-                />
+	const handlePlayAlbum = () => {
+		if (!currentAlbum) return;
 
-                {/* Content */}
+		const isCurrentAlbumPlaying = currentAlbum?.songs.some((song) => song._id === currentSong?._id);
+		if (isCurrentAlbumPlaying) togglePlay();
+		else {
+			// start playing the album from the beginning
+			playAlbum(currentAlbum?.songs, 0);
+		}
+	};
+
+	const handlePlaySong = (index: number) => {
+		if (!currentAlbum) return;
+
+		playAlbum(currentAlbum?.songs, index);
+	};
+
+	return (
+		<div className='h-full'>
+			<ScrollArea className='h-full rounded-md'>
+				{/* Main Content */}
+				<div className='relative min-h-full'>
+					{/* bg gradient */}
+					<div
+						className='absolute inset-0 bg-gradient-to-b from-[#008BF0]/80 via-zinc-900/80
+					 to-zinc-900 pointer-events-none'
+						aria-hidden='true'
+					/>
+
+					{/* Content */}
 					<div className='relative z-10'>
 						<div className='flex p-6 gap-6 pb-8'>
 							<img
@@ -53,23 +71,28 @@ const AlbumPage = () => {
 							</div>
 						</div>
 
-                        {/* play button */}
+						{/* play button */}
 						<div className='px-6 pb-4 flex items-center gap-6'>
 							<Button
+								onClick={handlePlayAlbum}
 								size='icon'
-								className='w-14 h-14 rounded-full bg-harmoniq hover:bg-harmoniqWHITE 
-                                hover:scale-105 transition-all'
+								className='w-14 h-14 rounded-full bg-harmoniq hover:bg-harmoniqWHITE
+                hover:scale-105 transition-all'
 							>
-								<Play className='h-7 w-7 text-white' />
+								{isPlaying && currentAlbum?.songs.some((song) => song._id === currentSong?._id) ? (
+									<Pause className='h-7 w-7 text-white' />
+								) : (
+									<Play className='h-7 w-7 text-white' />
+								)}
 							</Button>
 						</div>
 
-                        {/* Table Section */}
+						{/* Table Section */}
 						<div className='bg-black/20 backdrop-blur-sm'>
 							{/* table header */}
 							<div
 								className='grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm 
-                                text-zinc-400 border-b border-white/5'
+            text-zinc-400 border-b border-white/5'
 							>
 								<div>#</div>
 								<div>Title</div>
@@ -78,42 +101,52 @@ const AlbumPage = () => {
 									<Clock className='h-4 w-4' />
 								</div>
 							</div>
-                        </div>
 
-                    {/* songs list */}
+							{/* songs list */}
 
 							<div className='px-6'>
 								<div className='space-y-2 py-4'>
-									{currentAlbum?.songs.map((song, index) => (
+									{currentAlbum?.songs.map((song, index) => {
+										const isCurrentSong = currentSong?._id === song._id;
+										return (
 											<div
 												key={song._id}
+												onClick={() => handlePlaySong(index)}
 												className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
-                                                text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
-                                                `}
+                     							 text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
+                      `}
 											>
-											<div className='flex items-center justify-center'>
-												<span className='group-hover:hidden'>{index + 1}</span>
-												<Play className='h-4 w-4 hidden group-hover:block' />
-											</div>
-
-											<div className='flex items-center gap-3'>
-												<img src={song.imageUrl} alt={song.title} className='size-10' />
-
-												<div>
-													<div className={`font-medium text-white`}>{song.title}</div>
-													<div>{song.artist}</div>
+												<div className='flex items-center justify-center'>
+													{isCurrentSong && isPlaying ? (
+														<div className='size-4 text-harmoniq'>♫</div>
+													) : (
+														<span className='group-hover:hidden'>{index + 1}</span>
+													)}
+													{!isCurrentSong && (
+														<Play className='h-4 w-4 hidden group-hover:block' />
+													)}
 												</div>
-											</div>
+
+												<div className='flex items-center gap-3'>
+													<img src={song.imageUrl} alt={song.title} className='size-10' />
+
+													<div>
+														<div className={`font-medium text-white`}>{song.title}</div>
+														<div>{song.artist}</div>
+													</div>
+												</div>
 												<div className='flex items-center'>{song.createdAt.split("T")[0]}</div>
 												<div className='flex items-center'>{formatDuration(song.duration)}</div>
 											</div>
-                                    ))}
+										);
+									})}
 								</div>
 							</div>
-                    </div>
-            </div>
-        </ScrollArea>   
-    </div>   
-;
+						</div>
+					</div>
+				</div>
+			</ScrollArea>
+		</div>
+	);
 };
 export default AlbumPage;
